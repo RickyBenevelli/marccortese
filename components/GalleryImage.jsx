ddebdd4f55853ec49b461b815154d57c4543f15f
Riccardo Benevelli <rickybenevelli@gmail.com>
2023-05-02 00:38:42 +0200

    aggiunta la possibilità di ingrandire le immagini nella media page

--- components/GalleryImage.jsx
+++ components/GalleryImage.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Image from 'next/image';
 
-function GalleryImage({ src, width, height }) {
+function GalleryImage({src, width, height }) {
   return (
-    <div className="relative ">
+    <div className="relative aspect-square cursor-pointer">
       {/* <div className="absolute inset-0 bg-gray-300" /> */}
-      <Image src={`${src}`} alt="" width={width} height={height} />
+      <Image src={`${src}`} alt="" fill style={{objectFit: 'cover'}}/>
     </div>
   );
 }
--- pages/media.js
+++ pages/media.js
@@ -9,10 +9,15 @@ import PageTitle from '@/components/PageTitle'
 import imageBackground from '@/public/images/Media.webp'
 import GalleryImage from '@/components/GalleryImage'
 
+import {GrClose} from 'react-icons/gr'
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+
 
 const Media = ({photos}) => {
   const [selectedPhoto, setSelectedPhoto] = useState(null);
-  // console.log(photos)
+  console.log("SELECTED: " + selectedPhoto)
+
+
   return (
     <PageLayout imageBackground={imageBackground}>
         <div className='w-full px-5 md:px-10'>
@@ -24,17 +29,35 @@ const Media = ({photos}) => {
           </div>
 
           <div className="w-full mx-auto pt-10">
-            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+            <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4">
               {photos.map((photo) => (
-                <GalleryImage
-                  key={photo.src}
-                  src={photo.src}
-                  width={photo.width}
-                  height={photo.height}
-                  onClick={() => setSelectedPhoto(photo)}
-                />
+                <div 
+                key={photo.src}
+                onClick={() => setSelectedPhoto(photo)}>
+                  <GalleryImage
+                    src={photo.src}
+                    width={photo.width}
+                    height={photo.height}
+                    />
+                </div>
               ))}
             </div>
+            
+            {selectedPhoto && (
+              <div className='absolute inset-0 bg-black bg-opacity-80 z-30'>
+                <div className="fixed inset-x-[10vw] inset-y-[12vh] flex justify-center items-center bg-opacity-80 z-50">
+                  <div className="relative w-full h-full m-auto">
+                    {/* <GrClose className='w-5 h-5 absolute top-0 text-white right-0 z-[55]' onClick={() => setSelectedPhoto(null)}/> */}
+                    {/* <button className="absolute top-0 right-0 m-4 text-white text-xl font-bold z-[55]" onClick={() => setSelectedPhoto(null)}>
+                      X
+                    </button> */}
+                    <AiOutlineClose className='w-5 h-5 absolute top-0 text-white right-0 z-[55] cursor-pointer' onClick={() => setSelectedPhoto(null)}/>
+                    <Image src={selectedPhoto.src} alt="" fill style={{objectFit: 'contain'}}/>
+                  </div>
+                </div>
+              </div>
+            )}
+
           </div>
 
         </div>
