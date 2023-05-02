60f49eabe2bc66aa8af0d03e0a2b777047245614
Riccardo Benevelli <rickybenevelli@gmail.com>
2023-05-02 13:46:57 +0200

    aggiunto bottone di download sopra le foto nell'about page

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Image from 'next/image'
 
 import PageLayout from '@/components/PageLayout'
 import PageTitle from '@/components/PageTitle'
 import DownloadButton from '@/components/DownloadButton'
+import download from "@/public/icons/Download.svg";
 
 import imageBackground from '@/public/images/About.webp'
 
+const photos = [
+    { url: '/about/1.webp', name: 'MarcCortese.jpg' },
+    { url: '/about/2.webp', name: 'MarcCortese.jpg' },
+    { url: '/about/3.webp', name: 'MarcCortese.jpg' },
+  ];
+  
+  const handleDownloadMultiple = () => {
+    photos.forEach((photo) => {
+      const link = document.createElement('a');
+      link.href = photo.url;
+      link.download = photo.name;
+      link.textContent = photo.name;
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    });
+  };
+
 const About = () => {
+    const [hoveredImage, setHoveredImage] = useState(null);
+
+    const handleDownload = (image) => {
+        const link = document.createElement('a');
+        link.href = image.url;
+        link.download = `${image.url}`;
+        link.click();
+    };
+
   return (
     <PageLayout imageBackground={imageBackground}>
         <div className='px-5 md:px-10'>
@@ -25,17 +53,41 @@ const About = () => {
 
             <DownloadButton text="DOWNLOAD PRESSKIT" file="/prova.pdf"/>
             <div className='flex flex-col sm:flex-row'>
-                <div className='sm:w-1/4 py-4 sm:p-5 sm:pl-0'>
-                    <Image src='/about/1.webp' width={1200} height={800} alt='Cover' className=''/>
-                </div>
-                <div className='sm:w-1/4 py-4 sm:p-5 sm:pl-0'>
-                    <Image src='/about/2.webp' width={1200} height={800} alt='Cover' className=''/>
-                </div>
-                <div className='sm:w-1/4 py-4 sm:p-5 sm:pl-0'>
-                    <Image src='/about/3.webp' width={1200} height={800} alt='Cover' className=''/>
-                </div>
+                {photos.map((image) => (
+                    <div key={image.url} 
+                        className='sm:w-1/4 py-4 sm:p-5 sm:pl-0' 
+                        onMouseEnter={() => setHoveredImage(image.url)} 
+                        onMouseLeave={() => setHoveredImage(null)}
+                        >
+                        <div 
+                            className={`relative flex flex-col items-center cursor-pointer`}
+                            onClick={() => handleDownload(image)}
+                            >
+
+                            <Image
+                                src={image.url}
+                                width={1200}
+                                height={800}
+                                alt={`Image ${image.name}`}
+                                className={`${hoveredImage === image.url ? 'brightness-50' : ""}`}
+                                />
+
+                            {hoveredImage === image.url && (
+
+                                <Image src={download} alt="" className="h-10 w-10 top-[50%] left-[50%] absolute translate-x-[-50%] translate-y-[-50%] brightness-100"/>
+                                
+                                )}
+                        </div>
+                    </div>
+                ))}
+                
             </div>
-            <DownloadButton text="DOWNLOAD FOTO" file="/prova.pdf"/>
+
+            <button onClick={handleDownloadMultiple} className="py-5 text-xl md:text-3xl flex flex-row items-center cursor-pointer uppercase italic">
+                <p>DOWNLOAD PHOTOS</p>
+                <Image src={download} alt="" className="h-5 w-5 m-5" />
+            </button>
+
         </div>
     </PageLayout>
   )
