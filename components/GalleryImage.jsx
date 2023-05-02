b20bbad17c22e3881064e7741a6229f92f6d2bfb
Riccardo Benevelli <rickybenevelli@gmail.com>
2023-05-02 08:21:38 +0200

    prova di caricamento dell'immagine

--- components/GalleryImage.jsx
+++ components/GalleryImage.jsx
@@ -4,9 +4,9 @@ import Image from 'next/image';
 function GalleryImage({src, width, height }) {
     const [isLoading, setIsLoading] = useState(true)
   return (
-    <div className="relative aspect-square cursor-pointer">
+    <div className={`relative aspect-square cursor-pointer duration-500 ease-in-out ${isLoading ? "blur-lg grayscale scale-105" : "blur-0 scale-100 grayscale-0"}`}>
       {/* <div className="absolute inset-0 bg-gray-300" /> */}
-      <Image src={`${src}`} alt="" fill style={{objectFit: 'cover'}} onLoadingComplete={() => setIsLoading(false)} className={`${isLoading ? "blur-lg grayscale scale-105" : "blur-0 scale-100 grayscale-0"}`}/>
+      <Image src={`${src}`} alt="" fill style={{objectFit: 'cover'}} onLoadingComplete={() => setIsLoading(false)}/>
     </div>
   );
 }
--- pages/media.js
+++ pages/media.js
@@ -49,10 +49,13 @@ const Media = ({photos}) => {
             
             {selectedPhoto && (
               <div className='absolute inset-0 bg-black bg-opacity-80 z-30'>
-                <div className="fixed inset-x-[10vw] inset-y-[12vh] flex justify-center items-center bg-opacity-80 z-50" onClick={() => setSelectedPhoto(null)}>
-                  <div onClick={() => setSelectedPhoto(null)} className={`relative w-full h-full m-auto ${isLoading ? "blur-xl grayscale scale-105" : "blur-0 scale-100 grayscale-0"}`}>
+                <div className={`fixed inset-x-[10vw] inset-y-[12vh] flex justify-center items-center bg-opacity-80 z-50 ${isLoading ? "blur-xl grayscale scale-105" : "blur-0 scale-100 grayscale-0"}`} onClick={() => setSelectedPhoto(null)}>
+                  
+                  <div onClick={() => setSelectedPhoto(null)} className={`relative w-full h-full m-auto `}>
+                    
                     <AiOutlineClose className='w-5 h-5 absolute top-0 text-white right-0 z-[55] cursor-pointer' onClick={() => removeSelectedPhoto}/>
                     <Image src={selectedPhoto.src} alt="" fill style={{objectFit: 'contain'}} onLoadingComplete={() => setIsLoading(false)} />
+                  
                   </div>
                 </div>
               </div>
