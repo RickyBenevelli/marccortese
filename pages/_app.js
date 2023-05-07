920ffff5763d14de58c69f6988bf6e7ac1650bed
Riccardo Benevelli <rickybenevelli@gmail.com>
2023-05-07 11:08:03 +0200

    commit fittizio

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
 import { Analytics } from '@vercel/analytics/react';
 import { GoogleAnalytics } from "nextjs-google-analytics";
+
 import Layout from '@/components/Layout'
 
 export default function App({ Component, pageProps }) {
